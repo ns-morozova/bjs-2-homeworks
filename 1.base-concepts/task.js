1a8fd764234ea84b3edd6a34784dec60ee94751a
Морозова Наталия <ns_morozova@mail.ru>
2023-10-05 22:20:15 +0300

    Первое задание исправлено 05.10.23

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,12 @@
 "use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  let D = b ** 2 - 4 * a * c;
-  if (D < 0) {
-    console.log ('Уравнение не имеет корней');
-    return false;
-  } else if (D == 0) {
+  let d = b ** 2 - 4 * a * c;
+  if (d == 0) {
     arr[0] = -b/(2 * a);
-  } else if (D > 0) {
-	  arr[0] = (-b + Math.sqrt(D)) / (2 * a);
-	  arr[1] = (-b - Math.sqrt(D)) / (2 * a);	  
+  } else if (d > 0) {
+	  arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+	  arr[1] = (-b - Math.sqrt(d)) / (2 * a);	  
   }
   return arr;
 }
@@ -18,12 +15,12 @@ let f = solveEquation(2, 5, -3);
 console.log('Корни уравнения: ' + f);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = percent/100/12; //преобразованная процентная ставка
-  let S = amount - contribution; //тело кредита = сумма кредита - первоначальный взнос
-  let monthlyPayment = S * (P + (P / (((1 + P) ** countMonths) - 1))); //ежемесячный платеж
+  let p = percent/100/12; //преобразованная процентная ставка
+  let s = amount - contribution; //тело кредита = сумма кредита - первоначальный взнос
+  let monthlyPayment = s * (p + (p / (((1 + p) ** countMonths) - 1))); //ежемесячный платеж
   let totalAmount = monthlyPayment * countMonths; //общая сумма, которую заплатит клиент
   return totalAmount;
 }
 
-let Mortgage = calculateTotalMortgage(10, 0, 50000, 12).toFixed(2);
+let Mortgage = +calculateTotalMortgage(10, 0, 50000, 12).toFixed(2);
 console.log ('Общая сумма, которую клиент должен заплатить: ' + Mortgage);
